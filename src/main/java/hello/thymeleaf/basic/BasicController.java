df43c8f219c2cdd49f4a67f850049a36ad79bf95
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-04-10 15:41:57 +0900

    study

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -86,6 +87,57 @@ public class BasicController {
     public String literal(Model model){
         model.addAttribute("data","Spring!");
         return "basic/literal";
+    }
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data","Spring!");
+        return "basic/comments";
+    }
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user",new User("UserA",10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA",10));
+        list.add(new User("userB",20));
+        list.add(new User("userC",30));
+
+        model.addAttribute("users",list);
     }
 
 
